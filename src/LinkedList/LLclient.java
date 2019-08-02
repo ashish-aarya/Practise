bdd150c2f5defc56efdecb89ac72d1406a2f7dc8
ashish-aarya <arya.ashish229@gmail.com>
2019-08-02 16:52:12 +0530

    Linked List

--- src/LinkedList/LLclient.java
+++ src/LinkedList/LLclient.java
@@ -1,8 +1,22 @@
 package LinkedList;
 
 public class LLclient {
-	public static void main(String[] args) {
-		LinkedList ll= new LinkedList();
-		}
+	public static void main(String[] args) throws Exception {
+		LinkedList ll = new LinkedList();
+		ll.display();
+		ll.addlast(10);
+		ll.addlast(20);
+		ll.addlast(30);
+		ll.addlast(40);
+		ll.display();
+		ll.addfirst(50);
+		ll.addlast(70);
+		ll.addfirst(60);
+		ll.display();
+		System.out.println(ll.getFirst());
+		System.out.println(ll.getLast());
+		System.out.println(ll.getAt(3));
+		
+	}
 
 }
--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,22 +1,97 @@
 package LinkedList;
+
 public class LinkedList {
 
-	private class node
-	{
+	private class node {
 		int data;
 		node next;
 	}
+
 	private node head;
 	private node tail;
 	private int size;
-	
-	public  void display ()
-	{
-		node temp=this.head;
-		while (temp!=null)
-		{
-			System.out.println(temp.data);
-			temp=temp.next;
+
+	public void display() {
+		node temp = this.head;
+		System.out.println("------------------");
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
 		}
+		System.out.println(".");
+		System.out.println("------------------");
+	}
+
+	public void addlast(int item) {
+		node nn = new node();
+		nn.data = item;
+		nn.next = null;
+		if (this.size >= 1) {
+			this.tail.next = nn;
+		}
+		if (this.size == 0) {
+			this.head = nn;
+			this.tail = nn;
+			this.size++;
+
+		} else {
+			this.tail = nn;
+			this.size++;
+		}
+	}
+
+	public void addfirst(int item) {
+		node nn = new node();
+		nn.data = item;
+		nn.next = null;
+		if (size >= 1) {
+			nn.next = this.head;
+		}
+		if (size == 0) {
+			this.head = nn;
+			this.tail = nn;
+			this.size++;
+		} else {
+			this.head = nn;
+			this.size++;
+		}
+	}
+
+	public int getFirst() throws Exception {
+		if (this.size == 0)
+			throw new Exception("LinkedList is empty");
+		return this.head.data;
+
+	}
+
+	public int getLast() throws Exception {
+		if (this.size == 0)
+			throw new Exception("LinkedList is Empty");
+		return this.tail.data;
+	}
+
+	public int getAt(int idx) throws Exception {
+		if (idx == 0)
+			throw new Exception("Linked List Empty");
+		if (idx < 0 || idx >= this.size)
+			throw new Exception("Invalid Index");
+		node temp = this.head;
+		 for (int i=0;i<idx;i++)
+		 {
+			 temp=temp.next;
+		 }
+		 return temp.data;
+	}
+	private node getNodeAt(int idx) throws Exception {
+		if (idx == 0)
+			throw new Exception("Linked List Empty");
+		if (idx < 0 || idx >= this.size)
+			throw new Exception("Invalid Index");
+		node temp = this.head;
+		 for (int i=0;i<idx;i++)
+		 {
+			 temp=temp.next;
+		 }
+		 return temp;
 	}
 }
