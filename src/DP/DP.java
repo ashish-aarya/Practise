413bc0cf553485b56707aae827076055c93c32d1
ashish-aarya <arya.ashish229@gmail.com>
2020-01-02 23:26:13 +0530

    Prac

--- src/DP/DP.java
+++ src/DP/DP.java
@@ -4,18 +4,17 @@ public class DP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(fibbo (6));
+		System.out.println(fibbo(6));
 
 	}
-public static  int fibbo (int n)
-{
-	if (n==0||n==1)
-	{
-		return n;
+
+	public static int fibbo(int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		}
+		int f1 = fibbo(n - 1);
+		int f2 = fibbo(n - 2);
+		int fib = f1 + f2;
+		return fib;
 	}
-int f1= fibbo(n-1);
-int f2=fibbo (n-2);
-int fib= f1+f2;
-return fib;
-}
 }
