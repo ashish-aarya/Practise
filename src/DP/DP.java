05171e2b77235bc8e70a2a7f60787c652ee5ed4d
ashish-aarya <arya.ashish229@gmail.com>
2020-01-07 23:15:50 +0530

    DP

--- src/DP/DP.java
+++ src/DP/DP.java
@@ -4,13 +4,13 @@ public class DP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 8;
+		int n = 30;
 //		System.out.println(fibbo(n));
-//		int[] s = new int[n + 1];
+	int[] s = new int[n + 1];
 //		System.out.println(fibboRS(n, s));
 //		System.out.println(fiboir(n, s));
-		System.out.println(cbp(0, 10));
-
+		System.out.println(cbp(0, n));
+		System.out.println(cbprs(0, n, s));
 	}
 
 	public static int fibbo(int n) {
@@ -54,6 +54,24 @@ public class DP {
 		{
 			 count=count+cbp(curr+i, end);	
 		}
+	return count;
+	}
+	public static int cbprs (int curr, int end , int [] strg)
+	{
+		if (curr==end)
+			return 1;
+		if (curr>end)
+			return 0;
+		
+		int count=0;
+		if (strg[curr]!=0)
+			return strg[curr];
+			
+		for (int i=1;i<=6;i++)
+		{
+			 count=count+cbprs(curr+i, end,strg);	
+		}
+		strg[curr]=count;
 	return count;
 	}
 	
